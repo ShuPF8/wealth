7d3ad7355b613819593c0ae8f3182eb898d67e8a
shupf@stargoto.com <shupf>
2018-09-13 14:10:58 +0800

    华宇分分彩

---
+++ src/main/java/com/spf/wealth/wealth/hu/WanDaFf.java
@@ -0,0 +1,145 @@
+package com.spf.wealth.wealth.hu;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import com.spf.utils.HttpUtil;
+import com.spf.utils.mail.MailSend;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 腾讯分分彩
+ */
+public class WanDaFf {
+    private Logger logger = LogManager.getLogger(WanDaFf.class);
+
+    SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss:SSS");
+
+    private String[] toMails = new String[]{"517292069@qq.com","1129711788@qq.com"};
+
+    private CloseableHttpClient client = null;
+
+    public WanDaFf() {
+        client = HttpUtil.getClient();
+        HttpUriRequest request = null;
+        String url = null, data = null;
+
+        url = "http://pay4.hbcchy.com/lotterytrend/chart/8";
+        request = new HttpGet(url);
+        try {
+            HttpUtil.execute(client, request, logger);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public JSONObject login(int nextqh) throws Exception {
+        logger.info("--------------------------- 开始查询 [第 {} 期] {} ---------------------------", nextqh, sdf.format(new Date()));
+        HttpUriRequest request = null;
+        String url = null, data = null;
+
+        Map<String, Object> map = new HashMap<>();
+        map.put("id","8");
+        map.put("pnum","30");
+
+        url = "http://pay4.hbcchy.com/lotterytrend/getsscchart";
+        request = HttpUtil.formRequest(url, map);
+        request.addHeader("User-Agent","Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.84 Safari/537.36");
+        request.addHeader("Referer","http://pay4.hbcchy.com/lotterytrend/chart/16");
+        request.addHeader("X-Requested-With","XMLHttpRequest");
+        request.addHeader("MS-ASPNETCORE-TOKEN","223b09d5-0df8-484d-ad07-c38e6a6a43b8");
+        request.addHeader("Host","pay4.hbcchy.com");
+        data = HttpUtil.execute(client, request, logger);
+
+        return JSON.parseObject(data);
+    }
+
+    public boolean execute(JSONObject json, Properties properties, int count) throws Exception {
+        boolean hflag = false, qflag = false;
+        JSONArray datas = json.getJSONArray("data");
+        String kjqh = datas.getJSONArray(datas.size() - 1).getString(0);
+        Integer qh = Integer.valueOf(kjqh.split("-")[1]);
+        String kjxn = datas.getJSONArray(datas.size() - 1).getString(1);
+
+        int nextqh = properties.getNextqh();
+        if (nextqh - qh == 1) { //是上一期
+            Thread.sleep(5000);
+            json = login(nextqh); //重新查询
+            return execute(json, properties,count);
+        }
+
+        properties.setNextqh(qh + 1);
+        String q2 = kjxn.substring(0,2);
+        String h2 = kjxn.substring(3,kjxn.length());
+
+        if (count == 1) {
+            logger.info("------------------------------ 开奖信息为:" + kjqh + " [" + kjxn + "]\n");
+        }
+
+        String title = properties.getShtj();
+        int hcount = properties.getHcount();
+        String myNum = properties.getMyNum();
+        if (!myNum.contains(h2)) {
+            properties.setHlz(0);
+            hcount++;
+            properties.setHcount(hcount);
+            hflag = true;
+            if (properties.gethMaxbzCount() < hcount) {
+                properties.sethMaxbzCount(hcount); //记录最大不中次数
+            }
+            logger.info(properties.getName() + "------------------------------ 后二 ["+properties.getShtj()+"] 已有 [ " + hcount + " ] 期不中");
+            if (hcount == properties.gethMax()) {
+                MailSend.sendMail(title + "-" + qh,"后二",properties.getName() + "后二["+properties.getShtj()+"]已有 " + hcount + " 期不中，开奖信息" +kjqh+ " " + kjxn + "! 投注号码：" + myNum, toMails);
+            }
+        }
+
+        int qcount = properties.getQcount();
+        if (!myNum.contains(q2)) {
+            properties.setQlz(0);
+            qcount++;
+            properties.setQcount(qcount);
+            if (properties.getqMaxbzCount() < qcount) {
+                properties.setqMaxbzCount(qcount); //记录最大不中次数
+            }
+            qflag = true;
+            logger.info(properties.getName() + "------------------------------ 前二 [" + properties.getShtj() + "] 已有 [ " + qcount + " ] 期不中");
+            if (qcount == properties.getqMax()) {
+                MailSend.sendMail(title + "-" + qh, "前二",properties.getName() + "前二["+properties.getShtj()+"]已有 " + qcount + " 期不中，开奖信息" +kjqh+ " " + kjxn + "! 投注号码：" + myNum, toMails);
+            }
+        }
+
+        int hlz = properties.getHlz();
+        if (!hflag) {
+            properties.setHcount(0);
+            hlz++;
+            properties.setHlz(hlz);
+            if (properties.gethMaxlzCount() < hlz) {
+                properties.sethMaxlzCount(hlz); //记录最大中次数
+            }
+        }
+
+        int qlz = properties.getQlz();
+        if (!qflag) {
+            properties.setQcount(0);
+            qlz++;
+            properties.setQlz(qlz);
+            if (properties.getqMaxlzCount() < qlz) {
+                properties.setqMaxlzCount(qlz); //记录最大中次数
+            }
+        }
+
+        //logger.info("------------------------------ 腾讯分分后二连中 : {} 次， 腾讯分分前二连中 ：{} 次",hlz,qlz);
+        //logger.info("\n------------------------------------------------------------ 执行结束 {}---------------------------------------------\n", sdf.format(new Date()));
+        return true;
+    }
+
+}
---
+++ src/main/java/com/spf/wealth/wealth/hu/WanDaFfTask.java
@@ -0,0 +1,127 @@
+package com.spf.wealth.wealth.hu;
+
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.Test;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * @author ShuPF
+ * @类说明：
+ * @date 2018-09-13 14:06
+ */
+public class WanDaFfTask {
+
+
+    private String path = this.getClass().getClassLoader().getResource("").getPath();
+    private SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+
+    private SimpleDateFormat _sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss:SSS");
+
+    private int indexItart = path.indexOf("/target");
+
+    private String NineNum = "02,03,04,05,06,07,08,09,13,14,15,16,17,19,20,24,25,26,28,30,31,35,37,39,40,41,42,46,47,48,49,50,51,52,53,57,58,59,60,61,62,64,68,69,70,71,73,75,79,80,82,84,85,86,91,93,94,95,96,97";
+
+    private String TenNum = "02,03,04,05,06,07,08,09,13,14,15,16,18,20,24,25,27,29,30,31,36,38,39,40,41,42,47,48,49,50,51,52,55,57,58,59,60,61,63,64,68,69,70,72,74,75,79,81,82,83,84,85,86,90,92,93,94,95,96,97";
+
+    private String SevenNum = "02 03 04 05 06 08 09 13 14 15 17 18 19 20 24 26 27 28 30 31 35 36 37 39 40 41 42 46 48 49 50 51 53 57 58 59 60 62 63 64 68 69 71 72 73 75 79 80 81 82 84 85 86 90 91 93 94 95 96 97";
+
+    private JSONObject json = new JSONObject();
+
+    private WanDaFf tengXunFf = new WanDaFf();
+
+    private Logger logger = LogManager.getLogger(TengXunFfTest.class);
+
+    @Test
+    public void execute() throws Exception {
+        int nextqh = 850;
+        if (nextqh == 1441) {
+            nextqh = 1;
+        }
+        Properties sevenProperties = new Properties("华宇分分", SevenNum,nextqh,6,6,"和7,11");
+        Properties nineProperties = new Properties("华宇分分", NineNum,nextqh,6,6,"和9,11");
+        Properties tenProperties = new Properties("华宇分分", TenNum,nextqh,6,6,"和8,10");
+
+        int count = 0;
+        while (true) {
+            count++;
+            long start = System.currentTimeMillis();
+
+            login(nextqh);
+
+            executeNine(count,nineProperties);
+            excuteSeven(count,sevenProperties);
+            excuteTen(count,tenProperties);
+
+            nextqh++;
+            logger.info("--------------------------------------- 执行结束 {}----------------------------------------\n", _sdf.format(new Date()));
+
+            long end = System.currentTimeMillis();
+            long sjc = (end - start) / 1000; //时间差 秒
+            if (sjc < 30) {
+                Thread.sleep((60 - sjc) / 2 * 1000);
+            }
+
+            do {
+                end = System.currentTimeMillis();
+            } while (end - start < 60000);
+
+        }
+    }
+
+    public void executeNine(int count, Properties properties) throws Exception {
+        path = path.substring(0, indexItart) + "\\log\\" + sdf.format(new Date()) + "-华宇分分[和9-11]统计.txt";
+
+        tengXunFf.execute(json, properties,1);
+
+        if (properties.getPrevHMaxBuCount() < properties.gethMaxbzCount() || properties.getPrevQMaxBuCount() < properties.getqMaxbzCount()) {
+            properties.setPrevHMaxBuCount(properties.gethMaxbzCount());
+            properties.setPrevQMaxBuCount(properties.getqMaxbzCount());
+            String content = _sdf.format(new Date()) + "\n 华宇分分[和9-11]今日统计：后二最大连中："+ properties.gethMaxlzCount() +"，后二最大不中："+properties.gethMaxbzCount()+"，前二最大连中："+properties.getqMaxlzCount()+"，前二最大不中：" + properties.getqMaxbzCount() +"\n";
+            Write.write(content, path,true);
+        }
+    }
+
+    public void excuteTen(int count, Properties properties) throws Exception{
+        path = path.substring(0, indexItart) + "\\log\\" + sdf.format(new Date()) + "-华宇分分[和8-10]统计.txt";
+
+        tengXunFf.execute(json, properties,2);
+
+        if (properties.getPrevHMaxBuCount() < properties.gethMaxbzCount() || properties.getPrevQMaxBuCount() < properties.getqMaxbzCount()) {
+            properties.setPrevHMaxBuCount(properties.gethMaxbzCount());
+            properties.setPrevQMaxBuCount(properties.getqMaxbzCount());
+            String content = _sdf.format(new Date()) + "\n 华宇分分[和8-10]今日统计：后二最大连中："+ properties.gethMaxlzCount() +"，后二最大不中："+properties.gethMaxbzCount()+"，前二最大连中："+properties.getqMaxlzCount()+"，前二最大不中：" + properties.getqMaxbzCount() + "\n";
+            Write.write(content, path,true);
+        }
+    }
+
+    public void excuteSeven(int count, Properties properties) throws Exception{
+        path = path.substring(0, indexItart) + "\\log\\" + sdf.format(new Date()) + "-华宇分分[和7-11]统计.txt";
+
+        tengXunFf.execute(json, properties,3);
+
+        if (properties.getPrevHMaxBuCount() < properties.gethMaxbzCount() || properties.getPrevQMaxBuCount() < properties.getqMaxbzCount()) {
+            properties.setPrevHMaxBuCount(properties.gethMaxbzCount());
+            properties.setPrevQMaxBuCount(properties.getqMaxbzCount());
+            String content = _sdf.format(new Date()) + "\n 华宇分分[和7-11]今日统计：后二最大连中："+ properties.gethMaxlzCount() +"，后二最大不中："+properties.gethMaxbzCount()+"，前二最大连中："+properties.getqMaxlzCount()+"，前二最大不中：" + properties.getqMaxbzCount() + "\n";
+            Write.write(content, path,true);
+        }
+    }
+
+    public void login(int nextqh) throws Exception {
+        Integer qh = 0;
+        do {
+            Thread.sleep(3000);
+            json = tengXunFf.login(nextqh);
+            JSONArray datas = json.getJSONArray("data");
+            String kjqh = datas.getJSONArray(datas.size() - 1).getString(0);
+            qh = Integer.valueOf(kjqh.split("-")[1]);
+        } while (nextqh - qh != 0);
+
+    }
+
+}
